9c61093f02902b98de597d746ab921213b72f98d
Kush Dalal <kushdalal007@gmail.com>
2023-12-08 13:12:18 -0500

    faster methods for c++

--- GenSpec.cpp
+++ GenSpec.cpp
@@ -8,16 +8,16 @@
 using namespace std;
 
 // read in spectrums.txt as a vector of vectors
-std::vector<std::vector<double> > ReadSpectrums(const std::string& filename) {
-    std::vector<std::vector<double> > result;
-    std::ifstream file(filename);
+vector<vector<double> > ReadSpectrums(const string& filename) {
+    vector<vector<double> > result;
+    ifstream file(filename);
 
     if (file.is_open()) {
-        std::string line;
-        while (std::getline(file, line)) {
-            std::stringstream ss(line);
+        string line;
+        while (getline(file, line)) {
+            stringstream ss(line);
             double value;
-            std::vector<double> numbers;
+            vector<double> numbers;
             char discard;
             double num;
 
@@ -38,7 +38,7 @@ std::vector<std::vector<double> > ReadSpectrums(const std::string& filename) {
     return result;
 }
 
-// multiply spectrums in a vector of vectors
+// multiply spectrums in a vector of vectors which are dynamically allocated
 vector<double> MultiplySpectrums(vector<vector<double> > spectrums)
 {
   vector<double> result;
@@ -51,6 +51,34 @@ vector<double> MultiplySpectrums(vector<vector<double> > spectrums)
 
   return result;
 }
+
+// multiply spectrums in a vector of vectors where the vectors are const to avoid copying
+vector<double> MultiplySpectrumsConst(const vector<vector<double> >& spectrums) {
+    const size_t size = spectrums[0].size();
+    vector<double> result = spectrums[0];
+
+    for (size_t i = 1; i < spectrums.size(); ++i) {
+        const vector<double>& spectrum = spectrums[i];
+        for (size_t j = 0; j < size; ++j) {
+            result[j] *= spectrum[j];
+        }
+    }
+
+    return result;
+}
+
+// multiply spectrums in a vector of vectors in place to avoid copying
+void MultiplySpectrumsInPlace(vector<vector<double> >& spectrums, vector<double>& result) {
+    const size_t size = spectrums[0].size();
+
+    for (size_t i = 1; i < spectrums.size(); ++i) {
+        const vector<double>& spectrum = spectrums[i];
+        for (size_t j = 0; j < size; ++j) {
+            result[j] *= spectrum[j];
+        }
+    }
+}
+
 
 
 // Program to read in a file of spectra and multiply each of them together
@@ -69,6 +97,15 @@ int main(int argc, char* argv[])
   vector<double> result = MultiplySpectrums(spectrums);
   auto stop = chrono::high_resolution_clock::now();
 
+  auto start_const = chrono::high_resolution_clock::now();
+  vector<double> result_const = MultiplySpectrumsConst(spectrums);
+  auto stop_const = chrono::high_resolution_clock::now();
+
+  auto start_inplace = chrono::high_resolution_clock::now();
+  vector<double> result_inplace = spectrums[0];
+  MultiplySpectrumsInPlace(spectrums, result);
+  auto stop_inplace = chrono::high_resolution_clock::now();
+
   // print out the result
   for (int i = 0; i < result.size(); i++) {
     cout << result[i] << " ";
@@ -76,10 +113,17 @@ int main(int argc, char* argv[])
 
   // print out the time taken
   auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+  auto duration_const = chrono::duration_cast<chrono::microseconds>(stop_const - start_const);
+  auto duration_inplace = chrono::duration_cast<chrono::microseconds>(stop_inplace - start_inplace);
 
   // Convert microseconds to seconds
   double seconds = duration.count() / 1000000.0;
+  double seconds_const = duration_const.count() / 1000000.0;
+  double seconds_inplace = duration_inplace.count() / 1000000.0;
+
   cout << endl << "Time taken by function: " << seconds << " seconds" << endl;
+  cout << "Time taken by function (const): " << seconds_const << " seconds" << endl;
+  cout << "Time taken by function (inplace): " << seconds_inplace << " seconds" << endl;
   return 0;
 }
 
